12f074d3b0a1a586c063a8aa1e346dbf501996b7
Ashley Perrin <ashley.perrin1@hotmail.com>
2024-10-19 12:12:22 +0100

    Add create task to menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('tasks_create')" :active="request()->routeIs('tasks_create')">
+                        {{ __('Create Task') }}
+                    </x-nav-link>
                 </div>
             </div>
 
